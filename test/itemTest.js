afd304831a0d9747ca5c7fbeb996e5fceac38803
Randhi Pratama Putra <randhi.pp@gmail.com>
2020-11-24 14:39:56 +0700

    chai disable https verification

--- test/itemTest.js
+++ test/itemTest.js
@@ -9,6 +9,13 @@ const app = require('./../bin/www');
 
 // Configure chai
 
+
+// (node:17643) Warning: Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' 
+// makes TLS connections and HTTPS requests insecure by disabling certificate verification.
+// https://stackoverflow.com/questions/41572681/set-accepted-ca-list-and-ignore-ssl-errors-with-chai-http
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
 chai.use(chaiHttp);
 chai.use(require('chai-json-schema'));
 
